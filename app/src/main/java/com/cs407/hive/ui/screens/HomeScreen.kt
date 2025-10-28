43e20e00b05f1bdabbb097d9c968263b4e336bde
akshayasomasundaram <akshayasoma@gmail.com>
2025-10-28 13:45:41 -0500

    Settings icon and ai bee added

--- app/src/main/java/com/cs407/hive/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/cs407/hive/ui/screens/HomeScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -130,9 +131,9 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                     modifier = Modifier.size(60.dp)
                 ) {
                     Icon(
-                        painter = painterResource(id=R.drawable.ic_settings_flower),
+                        painter = painterResource(id=R.drawable.settings_flower),
                         contentDescription = "Settings",
-                        tint = MaterialTheme.colorScheme.onSecondary,
+                        tint = Color.Unspecified,
                         modifier = Modifier.size(50.dp)
 
                     )
