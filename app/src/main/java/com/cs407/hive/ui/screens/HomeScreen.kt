ae2b4f288bc7f524395751ab652e966188a005ef
Kavya Dewangan <kdewangan@wisc.edu>
2025-11-15 01:22:41 -0600

    Home Screen UI look changed and added ellipses to hardcoded username if it overflows

--- app/src/main/java/com/cs407/hive/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/cs407/hive/ui/screens/HomeScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,6 +54,15 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                 onNavigateToSettings: () -> Unit,
                 onNavigateToLeaderboard: () -> Unit
                 ){
+    val userName = "UserName" //hardcoded needs to be connected to the db
+//    val maxDisplayLength = 6
+//    val truncatedName = if (userName.length > maxDisplayLength) {
+//        // Truncate and add ellipsis
+//        userName.substring(0, maxDisplayLength) + "..."
+//    } else {
+//        userName
+//    }
+
     val CooperBt = FontFamily(
         Font(R.font.cooper_bt_bold)
     )
@@ -62,184 +72,6 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.background)
     ) {
-
-//        // Greeting Section
-//        Column(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(horizontal = 30.dp, vertical = 50.dp),
-//            horizontalAlignment = Alignment.Start
-//        ) {
-//            Row {
-//                Text(
-//                    text = "Hello!",
-//                    fontSize = 40.sp,
-//                    fontFamily = CooperBt,
-//                    color = MaterialTheme.colorScheme.onPrimaryContainer
-//                )
-//                Text(
-//                    text = " Name",
-//                    fontSize = 40.sp,
-//                    fontFamily = CooperBt,
-//                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-//                    fontStyle = FontStyle.Italic
-//                )
-//            }
-//
-//            Spacer(modifier = Modifier.height(10.dp))
-//
-//            Text(
-//                text = "Happy Hive, Happy Life",
-//                fontSize = 22.sp,
-//                color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f),
-//                fontWeight = FontWeight.Bold,
-//                fontStyle = FontStyle.Italic
-//            )
-//
-//            Spacer(modifier = Modifier.height(50.dp))
-//
-//
-//            // Buttons Box (fixed vertical position)
-//            Box(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    //.align(Alignment.TopCenter)
-//                    //.offset(y = 200.dp) // adjust distance from top for consistent placement
-//                    .background(
-//                        color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.5f),
-//                        shape = RoundedCornerShape(12.dp)
-//                    )
-//                    .padding(horizontal = 20.dp, vertical = 20.dp),
-//                contentAlignment = Alignment.Center
-//            ) {
-//                Column(
-//                    verticalArrangement = Arrangement.spacedBy(20.dp),
-//                    modifier = Modifier.fillMaxWidth()
-//                ) {
-//                    val buttons = listOf(
-//                        Triple("CHORES", R.drawable.chores_icon, onNavigateToChores),
-//                        Triple("GROCERY", R.drawable.grocery_icon, onNavigateToGrocery),
-//                        Triple("RECIPES", R.drawable.recipes_icon, onNavigateToRecipe)
-//                    )
-//
-//                    val iconOffsets = mapOf(
-//                        "CHORES" to (-12).dp,
-//                        "GROCERY" to (-23).dp,
-//                        "RECIPES" to 0.dp
-//                    )
-//
-//                    buttons.forEach { (text, iconRes, onClickAction) ->
-//                        Button(
-//                            onClick = { onClickAction() },
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .height(140.dp),
-//                            shape = RoundedCornerShape(24.dp),
-//                            colors = ButtonDefaults.buttonColors(
-//                                containerColor = MaterialTheme.colorScheme.onPrimary
-//                            ),
-//                            contentPadding = PaddingValues(10.dp)
-//                        ) {
-//                            Box(modifier = Modifier.fillMaxSize()) {
-//                                Icon(
-//                                    painter = painterResource(id = iconRes),
-//                                    contentDescription = text,
-//                                    tint = Color.Unspecified,
-//                                    modifier = Modifier
-//                                        .size(140.dp)
-//                                        .align(Alignment.CenterStart)
-//                                        .offset(x = iconOffsets[text] ?: 0.dp)
-//                                )
-//
-//                                Text(
-//                                    text = text,
-//                                    fontFamily = CooperBt,
-//                                    fontWeight = FontWeight.Bold,
-//                                    fontSize = 30.sp,
-//                                    color = MaterialTheme.colorScheme.onSecondary,
-//                                    modifier = Modifier.align(Alignment.BottomEnd)
-//                                )
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                // Bee on bottom_left of the box
-//                Icon(
-//                    painter = painterResource(id = R.drawable.home_left_bee),
-//                    contentDescription = "Left Bee",
-//                    tint = Color.Unspecified,
-//                    modifier = Modifier
-//                        .size(100.dp)
-//                        .align(Alignment.TopStart)
-//                        .offset(x = (-51).dp, y = 423.dp)
-//                )
-//
-//                // Bee on top_right of the box
-//                Icon(
-//                    painter = painterResource(id = R.drawable.home_right_bee),
-//                    contentDescription = "Right Bee",
-//                    tint = Color.Unspecified,
-//                    modifier = Modifier
-//                        .size(100.dp)
-//                        .align(Alignment.TopEnd)
-//                        .offset(x = 40.dp, y = (-90).dp)
-//                )
-//            }
-//        }
-//
-//
-//        BottomAppBar(
-//            modifier = Modifier.align(Alignment.BottomCenter),
-//            containerColor = MaterialTheme.colorScheme.onTertiary,
-//            tonalElevation = 8.dp
-//        ) {
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(horizontal = 20.dp),
-//                horizontalArrangement = Arrangement.SpaceBetween,
-//                verticalAlignment = Alignment.CenterVertically
-//            ) {
-//                // Settings Button
-//                Button(
-//                    onClick = { onNavigateToSettings()},
-//                    shape = CircleShape,
-//                    contentPadding = PaddingValues(0.dp),
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = MaterialTheme.colorScheme.onPrimary
-//                    ),
-//                    modifier = Modifier.size(60.dp)
-//                ) {
-//                    Icon(
-//                        painter = painterResource(id=R.drawable.settings_flower),
-//                        contentDescription = "Settings",
-//                        tint = Color.Unspecified,
-//                        modifier = Modifier.size(50.dp)
-//
-//                    )
-//                }
-//
-//                // Leaderboard Button
-//                Button(
-//                    onClick = { onNavigateToLeaderboard() },
-//                    shape = CircleShape,
-//                    contentPadding = PaddingValues(0.dp),
-//                    colors = ButtonDefaults.buttonColors(
-//                        containerColor = MaterialTheme.colorScheme.onPrimary
-//                    ),
-//                    modifier = Modifier.size(60.dp)
-//                ) {
-//                    Icon(
-//                        painter = painterResource(id=R.drawable.ic_leaderboard),
-//                        contentDescription = "Leaderboard",
-//                        tint = MaterialTheme.colorScheme.onSecondary,
-//                        modifier = Modifier.size(50.dp)
-//                    )
-//                }
-//            }
-//        }
-//    }
 
         Scaffold(
             bottomBar = {
@@ -312,18 +144,22 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(horizontal = 30.dp, vertical = 50.dp),
+                            .padding(horizontal = 30.dp)
+                            .padding(top = 40.dp, bottom = 40.dp),
                         horizontalAlignment = Alignment.Start
                     ) {
                         Row {
                             Text(
-                                text = "Hello!",
+                                text = "Hello! ",
                                 fontSize = 40.sp,
                                 fontFamily = CooperBt,
                                 color = MaterialTheme.colorScheme.onPrimaryContainer
                             )
                             Text(
-                                text = " Name",
+                                text = userName,
+                                modifier = Modifier.weight(1f),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
                                 fontSize = 40.sp,
                                 fontFamily = CooperBt,
                                 color = MaterialTheme.colorScheme.onPrimaryContainer,
@@ -341,8 +177,7 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                             fontStyle = FontStyle.Italic
                         )
 
-                        Spacer(modifier = Modifier.height(50.dp))
-
+                        Spacer(modifier = Modifier.height(40.dp))
 
                         // Buttons Box
                         Box(
@@ -415,8 +250,8 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                                 tint = Color.Unspecified,
                                 modifier = Modifier
                                     .size(100.dp)
-                                    .align(Alignment.TopStart)
-                                    .offset(x = (-51).dp, y = 417.dp)
+                                    .align(Alignment.BottomStart)
+                                    .offset(x = (-51).dp, y = 40.dp)
                             )
 
                             // Right Bee
@@ -429,6 +264,7 @@ fun HomeScreen (onNavigateToChores: () -> Unit,
                                     .align(Alignment.TopEnd)
                                     .offset(x = 40.dp, y = (-90).dp)
                             )
+
                         }
                     }
                 }
